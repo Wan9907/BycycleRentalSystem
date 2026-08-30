b833251e730cc4c5c338ada26ec3d288cae35f11
Lhong99 <145998323+Lhong99@users.noreply.github.com>
2026-08-30 21:18:13 +0800

    Update project

--- Bicycle.cpp
+++ Bicycle.cpp
@@ -309,7 +309,7 @@ void rentBike(vector<Bicycle>& fleet, vector<Rental>& rentals, vector<Schedule>&
                 saveFleetToFile(fleet);
             }
 
-            break;
+          
         }
     
 
